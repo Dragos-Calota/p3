1b58bcefb6b4009c215147bdbebfb73938525d77
WildJavaScript <calotadra@gmail.com>
2023-03-02 22:57:09 +0200

    p3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import Paper from "@mui/material/Paper";
+import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  DayView,
+  WeekView,
+  Appointments,
+  Toolbar,
+  ViewSwitcher,
+  DateNavigator,
+  TodayButton,
+  AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
+  DragDropProvider,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
-function App() {
+// import appointments from "./data";
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  const currentDate = new Date();
+
+  const commitChanges = useCallback(
+    ({ added, changed, deleted }) => {
+      if (added) {
+        const startingAddedId =
+          data.length > 0 ? data[data.length - 1].id + 1 : 0;
+
+        setData((data) => [...data, { id: startingAddedId, ...added }]);
+      }
+      if (changed) {
+        setData(
+          data.map((appointment) =>
+            changed[appointment.id]
+              ? { ...appointment, ...changed[appointment.id] }
+              : appointment
+          )
+        );
+      }
+      if (deleted !== undefined) {
+        setData(data.filter((appointment) => appointment.id !== deleted));
+      }
+    },
+    [data]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Paper>
+        <Scheduler data={data} firstDayOfWeek={1} locale={"ro-RO"}>
+          <ViewState
+            defaultCurrentDate={currentDate}
+            defaultCurrentViewName="Week"
+          />
+          <EditingState onCommitChanges={commitChanges} />
+          <EditRecurrenceMenu />
+          <DayView startDayHour={8} endDayHour={22} />
+          <WeekView startDayHour={8} endDayHour={22} />
+          <Toolbar />
+          <DateNavigator />
+          <TodayButton />
+          <ViewSwitcher />
+          <Appointments />
+          <AppointmentTooltip showCloseButton showOpenButton />
+          <AppointmentForm />
+          <DragDropProvider />
+        </Scheduler>
+      </Paper>
+      ;
+    </React.Fragment>
   );
 }
-
-export default App;
